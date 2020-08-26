b27c63dd2f2c1a26a739f2bc21add00308ffb920
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-26 13:12:23 -0400

    Added loop

--- patron3.rb
+++ patron3.rb
@@ -1,3 +1,15 @@
 #Input variable
 n = ARGV[0].to_i
 
+#Loop
+n.times do |i|
+    if i % 3 == 0
+        print ".."
+    elsif i % 3 == 1
+        print "**"
+    else 
+        print "||"
+    end
+end
+puts "\n"
+
